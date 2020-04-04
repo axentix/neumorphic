ff7fc6ba90f16ea5aa44ec066d994f3026710dfe
Stallos11 <vincentdelev@yahoo.fr>
2020-04-04 16:58:26 +0200

    handle focus-blur & mousedown-mouseup events

--- src/js/core.js
+++ src/js/core.js
@@ -1,50 +1,75 @@
 const NeuAxentix = (() => {
-  const withStateElements = document.querySelectorAll('[data-neu-click]');
+  const neuClickElements = document.querySelectorAll('[data-neu-click]');
+  const neuFocusElements = document.querySelectorAll('[data-neu-focus]');
+  const neuClickedElements = document.querySelectorAll('[data-neu-clicked]');
   const stateList = ['neu-bordered', 'neu-flat', 'neu-pressed', 'neu-concave', 'neu-convex'];
 
   /**
-   * Toggle neu-click state
+   * Toggle neu-clicked state
    */
-  function _toggle() {
-    if (this.dataset.neuClick && !stateList.includes(this.dataset.neuClick)) {
-      console.error('Error: invalid classname in data-neu-click.');
+  function _toggle(datasetName) {
+    if (this.dataset[datasetName] && !stateList.includes(this.dataset[datasetName])) {
+      console.error('Error: invalid classname in data-neu-...');
       return;
     }
 
-    const nextClass = this.dataset.neuClick;
-    if (this.neuClickCurrClass) {
-      this.classList.remove(this.neuClickCurrClass);
-      this.dataset.neuClick = this.neuClickCurrClass;
+    const nextClass = this.dataset[datasetName];
+    if (this.neuCurrClass) {
+      this.classList.remove(this.neuCurrClass);
+      this.dataset[datasetName] = this.neuCurrClass;
     } else {
-      this.dataset.neuClick = '';
+      this.dataset[datasetName] = '';
     }
 
     if (nextClass) {
       this.classList.add(nextClass);
-      this.neuClickCurrClass = nextClass;
+      this.neuCurrClass = nextClass;
     } else {
-      this.neuClickCurrClass = '';
+      this.neuCurrClass = '';
     }
+  }
+
+  /**
+   * Setup neumorph classes
+   */
+  function _setupClasses(el) {
+    let classList = el.className.split(' ');
+
+    const neumorphClasses = classList.filter(className => stateList.includes(className));
+    if (neumorphClasses.length >= 2) {
+      const newClasses = classList.filter(className => !neumorphClasses.includes(className));
+      newClasses.push(neumorphClasses[0]);
+      el.className = newClasses.join(' ');
+    }
+
+    el.neuCurrClass = neumorphClasses[0] || '';
   }
 
   /**
    * Setup neu-click elements
    */
   function setup() {
-    withStateElements.forEach(el => {
-      let classList = el.className.split(' ');
-
-      const neumorphClasses = classList.filter(className => stateList.includes(className));
-      if (neumorphClasses.length >= 2) {
-        const newClasses = classList.filter(className => !neumorphClasses.includes(className));
-        newClasses.push(neumorphClasses[0]);
-        el.className = newClasses.join(' ');
-      }
-
-      el.neuClickCurrClass = neumorphClasses[0] || '';
-      el.toggleRef = _toggle.bind(el);
+    neuClickedElements.forEach(el => {
+      _setupClasses(el);
+      el.toggleRef = _toggle.bind(el, 'neuClicked');
 
       el.addEventListener('click', el.toggleRef);
+    });
+
+    neuClickElements.forEach(el => {
+      _setupClasses(el);
+      el.toggleRef = _toggle.bind(el, 'neuClick');
+
+      el.addEventListener('mousedown', el.toggleRef);
+      el.addEventListener('mouseup', el.toggleRef);
+    });
+
+    neuFocusElements.forEach(el => {
+      _setupClasses(el);
+      el.toggleRef = _toggle.bind(el, 'neuFocus');
+
+      el.addEventListener('focus', el.toggleRef);
+      el.addEventListener('blur', el.toggleRef);
     });
   }
 
